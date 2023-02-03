0abaeca394312bb9881c4b63b0fe56e2dc1af186
lordfundarbyrd <lordfundarbyrd@gmail.com>
2023-02-03 16:10:00 -0500

    Dark mode

---
+++ main.js
@@ -0,0 +1,20 @@
+lightMode = true;
+
+// changes the page between light and dark mode
+function buttonClick() {
+    switch(lightMode) {
+        case true:
+            document.body.style.backgroundColor = "black";
+            document.body.style.color = "white";
+            document.getElementById("modeToggle").innerHTML="Light mode"
+            lightMode = false;
+            break;
+        case false:
+            document.body.style.backgroundColor = "white";
+            document.body.style.color = "black";
+            document.getElementById("modeToggle").innerHTML="Dark mode"
+            lightMode = true;
+            break;
+        default: return; // should never get here
+    }
+}
